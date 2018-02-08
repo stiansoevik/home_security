5337014479421b00895377d77fde43faf498fcd5
Stian Søvik <stian.s@gmail.com>
2018-02-08 22:39:20 +0100

    Create state and transition objects from config

--- state_machine.py
+++ state_machine.py
@@ -4,14 +4,32 @@ import pprint
 
 class StateMachine:
     def __init__(self, config_file = 'state_machine_config.json'):
+        self.states = []
         self.sm_dict = self.load(config_file)
 
     def load(self, config_file):
-        print('Loading {}'.format(config_file))
+        print("Loading {}".format(config_file))
         with open(config_file) as file:
             config_dict = json.loads(file.read())
         pprint.pprint(config_dict)
-        return config_dict
+
+        # Create all states first, so transitions can find a valid state
+        self.states = [State(name) for name in config_dict.get('states', [])]
+
+        # Add transitions
+        for state_name, properties in config_dict.get('states',[]).iteritems():
+            from_state = [s for s in self.states if s.name == state_name][0]
+            for t in properties.get('transitions', []):
+                transition = Transition(
+                        event = t.get('event'),
+                        condition = t.get('condition'),
+                        to_state = [s for s in self.states if s.name == t.get('to_state')][0]
+                        )
+                from_state.add_transition(transition)
+
+    def __str__(self):
+        return "\n".join([str(s) for s in self.states])
+
 
 class State:
     def __init__(self, name):
@@ -44,4 +62,5 @@ disarm_transition = Transition(to_state = disarmed_state)
 disarmed_state.add_transition(arm_transition)
 armed_state.add_transition(disarm_transition)
 
-print(disarmed_state)
+#print(disarmed_state)
+print(sm)

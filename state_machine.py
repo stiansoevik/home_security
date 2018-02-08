736d6a9f4fd038d6064ba5939572c51051a42711
Stian Søvik <stian.s@gmail.com>
2018-02-08 22:10:57 +0100

    Test using objects for states and transitions

--- state_machine.py
+++ state_machine.py
@@ -3,14 +3,45 @@ import json
 import pprint
 
 class StateMachine:
-    def load(self, filename):
-        print('Loading {}'.format(filename))
-        with open(filename) as file:
-            self.sm_dict = json.loads(file.read())
-        pprint.pprint(self.sm_dict)
+    def __init__(self, config_file = 'state_machine_config.json'):
+        self.sm_dict = self.load(config_file)
+
+    def load(self, config_file):
+        print('Loading {}'.format(config_file))
+        with open(config_file) as file:
+            config_dict = json.loads(file.read())
+        pprint.pprint(config_dict)
+        return config_dict
 
 class State:
+    def __init__(self, name):
+        self.name = name
+        self.transitions = []
 
+    def add_transition(self, transition):
+        self.transitions.append(transition)
+
+    def __str__(self):
+        return "{}:\n{}".format(self.name, "\n".join([str(s) for s in self.transitions]))
+
+class Transition:
+    def __init__(self, event = None, condition = None, to_state = None):
+        self.event = event
+        self.condition = condition
+        self.to_state = to_state
+
+    def __str__(self):
+        return "@{} [{}]: -> {}".format(self.event, self.condition, self.to_state.name)
 
 sm = StateMachine()
-sm.load('state_machine_config.json')
+
+disarmed_state = State("DISARMED")
+armed_state = State("ARMED")
+
+arm_transition = Transition(to_state = armed_state)
+disarm_transition = Transition(to_state = disarmed_state)
+
+disarmed_state.add_transition(arm_transition)
+armed_state.add_transition(disarm_transition)
+
+print(disarmed_state)

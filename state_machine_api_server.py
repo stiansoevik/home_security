519839d9f8b8b5ccdb47a3f90d07282634952370
Stian Søvik <stian.s@gmail.com>
2018-02-13 23:27:05 +0100

    Support configuring config file from command line

--- state_machine_api_server.py
+++ state_machine_api_server.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+
+import argparse
 import flask
 import logging
 import state_machine
@@ -5,7 +8,7 @@ import state_machine
 logging.basicConfig(level = logging.INFO)
 
 app = flask.Flask(__name__)
-sm = state_machine.StateMachine()
+sm = None
 
 @app.route('/send_event/<event>', methods = ['POST'])
 def send_event(event):
@@ -23,4 +26,18 @@ def send_event(event):
 
     return flask.json.jsonify(state=response.state, message=response.message), code
 
+def main():
+    parser = argparse.ArgumentParser(description = "State machine server")
+    parser.add_argument('--config', help = "Configuration file")
+    args = parser.parse_args()
 
+    global sm
+    if args.config:
+        sm = state_machine.StateMachine(config_file = args.config)
+    else:
+        sm = state_machine.StateMachine()
+
+    app.run()
+
+if __name__ == '__main__':
+    main()
